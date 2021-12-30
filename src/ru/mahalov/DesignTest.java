9cb4e62020e31d0eb15698a9b5208d5fb0c576cc
Maxim Mahalov <macklay999@mail.ru>
2021-12-30 22:52:48 +0500

    Реализация потокобезопасных Singleton: SynchronizeAccessor, DoubleChecking и ClassHolder

--- src/ru/mahalov/DesignTest.java
+++ src/ru/mahalov/DesignTest.java
@@ -1,15 +1,14 @@
 package ru.mahalov;
 
-import ru.mahalov.singleton.impl.MusicSourceLazySingleton;
-import ru.mahalov.singleton.impl.MusicSourceSimpleSingleton;
+import ru.mahalov.singleton.impl.*;
 import ru.mahalov.singleton.MusicSourceSingleton;
 
 public class DesignTest {
 
     public static void main(String[] args) {
 
-        MusicSourceSingleton musicSource = MusicSourceLazySingleton.getInstance();
-        MusicSourceSingleton musicSource_one = MusicSourceLazySingleton.getInstance();
+        MusicSourceSingleton musicSource = MusicSourceClassHolderSingleton.getInstance();
+        MusicSourceSingleton musicSource_one = MusicSourceClassHolderSingleton.getInstance();
 
         System.out.println(musicSource);
         System.out.println(musicSource == musicSource_one);
--- src/ru/mahalov/singleton/MusicSourceSingleton.java
+++ src/ru/mahalov/singleton/MusicSourceSingleton.java
@@ -1,7 +1,10 @@
 package ru.mahalov.singleton;
 
 public interface MusicSourceSingleton {
+
     static MusicSourceSingleton getInstance() {
         return null;
     }
+
+
 }
